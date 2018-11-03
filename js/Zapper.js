6c8a1bcb6400610c7877366a4227028418da90fb
atlimp <amp17@hi.is>
2018-11-03 16:39:35 +0000

    laga zappers

--- js/Zapper.js
+++ js/Zapper.js
@@ -6,4 +6,23 @@ class Zapper extends Obstacle {
     this.spacing = util.randRange(100, 150);
   }
 
+  render(ctx) {
+    ctx.save();
+
+    // Matrix magic for rotation
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.rotation);
+    ctx.translate(-this.x, -this.y);
+
+    // Draw yellow electricity
+    ctx.fillStyle = '#ffff00';
+    util.fillBox(ctx, this.x, this.y, this.spacing, 10);
+
+    // Draw grey circles on either side
+    ctx.fillStyle = '#666666';
+    util.fillCircle(ctx, this.x, this.y, this.halfWidth);
+    util.fillCircle(ctx, this.x + this.spacing, this.y, this.halfWidth);
+
+    ctx.restore();
+  }
 }
